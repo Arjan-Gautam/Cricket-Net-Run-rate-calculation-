1c3addc4cfd09770620d252b14aff0d29110aad4
Arjan-Gautam <aarjangautam40@gmail.com>
2025-06-07 12:11:07 +0545

    Update newfile.py
    Beginner friendly code to calculate Net Run rate in cricket.

--- newfile.py
+++ newfile.py
@@ -1,37 +1,10 @@
-print("1 - Addition")
-print("2 - Subtraction")
-print("3 - Multiplication")
-print("4 - Division")
-print("5 - Factorial")
-
-Option = int(input("Choose an operation (1-5): "))
-
-if Option in [1, 2, 3, 4]:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    
-    if Option == 1:
-        result = num1 + num2
-    elif Option == 2: 
-        result = num1 - num2
-    elif Option == 3:
-        result = num1 * num2
-    elif Option == 4:
-        if num2 != 0:
-            result = num1 // num2  
-        else:
-            result = "Undefined (division by zero)"
-    print("The result of the operation is: {}".format(result))
-
-elif Option == 5:
-    n = int(input("Enter a number: "))
-    factorial = 1
-    if n < 0:
-        print("Factorial is not defined for negative numbers.")
-    else:
-        for i in range(1, n+1):
-            factorial *= i
-        print(f"The factorial of {n} is {factorial}")
-
-else:
-    print(f"{Option} is an invalid option.")
+RunScored = int(input("Run scored: "))
+OverCompleted = float(input("Over completed: "))
+full_over = int(OverCompleted)
+extra_balls = round((OverCompleted - full_over) * 10)
+if extra_balls > 5:
+	print("Invalid Input: One over has just 6 balls (0-5)")
+else:	
+      total_balls = full_over * 6 + extra_balls
+      Runrate = (RunScored / total_balls)*6
+      print(f"Net Runrate is {Runrate:.2f} Runs per over")
